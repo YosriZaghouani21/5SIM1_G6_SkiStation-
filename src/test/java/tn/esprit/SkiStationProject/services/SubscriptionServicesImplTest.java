72674dc5c281ad4818cf4aa2a318bacf5732fa3f
bahaeddinelakhzouri <62963627+bahalakhzouri@users.noreply.github.com>
2023-11-15 22:20:44 +0100

    Docker-compose

--- src/test/java/tn/esprit/SkiStationProject/services/SubscriptionServicesImplTest.java
+++ src/test/java/tn/esprit/SkiStationProject/services/SubscriptionServicesImplTest.java
@@ -17,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
-@ExtendWith(MockitoExtension.class) 
+@ExtendWith(MockitoExtension.class)
 public class SubscriptionServicesImplTest {
 
     @Mock
